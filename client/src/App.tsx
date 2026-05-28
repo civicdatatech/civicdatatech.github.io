5e42d0f2b7e1ee837963a1b59a4853cf41d53c78
Virginia Dooley <7017118+VirginiaDooley@users.noreply.github.com>
2026-05-28 16:14:44 +0100

    Hide Volunteer and Banner sections until the next event

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,12 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Banner } from "@/components/ui/banner";
+// import { Banner } from "@/components/ui/banner";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import { GetInvolved } from "@/pages/GetInvolved";
 import { Strategy } from "@/pages/Strategy";
-import { Volunteer } from "@/pages/Volunteer";
+// import { Volunteer } from "@/pages/Volunteer";
 import { Navbar } from "@/components/sections/Navbar";
 import { Footer } from "@/components/sections/Footer";
 
@@ -18,13 +18,13 @@ function AppRouter() {
     <WouterRouter>
       <div className="min-h-screen bg-background flex flex-col">
         <Navbar />
-        <Banner />
+        {/* <Banner /> */}
         <main className="flex-grow pt-28">
           <Switch>
             <Route path="/" component={Home} />
             <Route path="/get-involved" component={GetInvolved} />
             <Route path="/strategy" component={Strategy} />
-            <Route path="/volunteer" component={Volunteer} />
+            {/* <Route path="/volunteer" component={Volunteer} /> */}
             <Route component={NotFound} />
           </Switch>
         </main>
